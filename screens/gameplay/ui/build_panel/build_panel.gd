96d4eb5f95cc2611fd2e4333b89539aab6746b70
Johan Hargne <johan.hargne@gmail.com>
2023-01-19 01:39:38 +0100

    Improved build panel visuals

--- screens/gameplay/ui/build_panel/build_panel.gd
+++ screens/gameplay/ui/build_panel/build_panel.gd
@@ -4,16 +4,19 @@ class_name BuildPanel
 signal started_dragging_object(buildOption)
 # Refs
 onready var warehouseButtonContainer = $GridContainer/Warehouses
-onready var warehouseButton = $GridContainer/Warehouses/Button
+onready var warehouseButton = $GridContainer/Warehouses/ButtonContainer/Button
+onready var warehouseAmountLabel = $GridContainer/Warehouses/AmountContainer/CenterContainer/Label
 onready var routeButtonContainer = $GridContainer/Routes
-onready var routeButton = $GridContainer/Routes/Button
+onready var routeButton = $GridContainer/Routes/ButtonContainer/Button
+onready var routeAmountLabel = $GridContainer/Routes/AmountContainer/CenterContainer/Label
 onready var cartButtonContainer = $GridContainer/Carts
-onready var cartButton = $GridContainer/Carts/Button
+onready var cartButton = $GridContainer/Carts/ButtonContainer/Button
+onready var cartAmountLabel = $GridContainer/Carts/AmountContainer/CenterContainer/Label
 
 func _ready():
   Utils.connect_signal(warehouseButton, "button_down", self, "initiate_dragging_warehouse")
 
-func get_buttoncontainer_by_type(buildOption) -> Node2D:
+func get_container_by_type(buildOption) -> Node2D:
   match buildOption:
     GameplayEnums.BuildOption.WAREHOUSE:
       return warehouseButtonContainer
@@ -23,29 +26,48 @@ func get_buttoncontainer_by_type(buildOption) -> Node2D:
        return cartButtonContainer
   return null
 
+func get_button_by_type(buildOption) -> Node2D:
+  match buildOption:
+    GameplayEnums.BuildOption.WAREHOUSE:
+      return warehouseButton
+    GameplayEnums.BuildOption.ROUTE:
+      return routeButton
+    GameplayEnums.BuildOption.CART:
+        return cartButton
+  return null
+
+func get_amountlabel_by_type(buildOption) -> Node2D:
+  match buildOption:
+    GameplayEnums.BuildOption.WAREHOUSE:
+      return warehouseAmountLabel
+    GameplayEnums.BuildOption.ROUTE:
+      return routeAmountLabel
+    GameplayEnums.BuildOption.CART:
+        return cartAmountLabel
+  return null
+
 func hide_build_option(buildOption) -> void:
-  var buttonContainer = get_buttoncontainer_by_type(buildOption)
-  if buttonContainer:
-    buttonContainer.visible = false
+  var container = get_container_by_type(buildOption)
+  if container:
+    container.visible = false
     
 func show_build_option(buildOption) -> void:
-  var buttonContainer = get_buttoncontainer_by_type(buildOption)
-  if buttonContainer:
-    buttonContainer.visible = true
+  var container = get_container_by_type(buildOption)
+  if container:
+    container.visible = true
 
 func set_build_option_amount(buildOption, amount) -> void:
-  var buttonContainer = get_buttoncontainer_by_type(buildOption)
-  if buttonContainer:
-    for c in buttonContainer.get_children():
-      if c is Label:
-        c.text = "x" + str(amount)
-      elif c is TextureButton:
-        if amount > 0 && c.disabled:
-          c.disabled = false
-          c.modulate.a = 1
-        elif amount <= 0 && !c.disabled:
-          c.disabled = true
-          c.modulate.a = 0.25
+  var button = get_button_by_type(buildOption)
+  if button:
+    if amount > 0 && button.disabled:
+      button.disabled = false
+      button.modulate.a = 1
+    elif amount <= 0 && !button.disabled:
+      button.disabled = true
+      button.modulate.a = 0.25
+  var amountLabel = get_amountlabel_by_type(buildOption)
+  if amountLabel:
+    amountLabel.text = "x" + str(amount)
 
 # Stock should consist of a BuildOption as key coupled with a value: { BuildOption.CART: 1 }
 func update_stock(stock) -> void:
--- screens/gameplay/ui/gameplay_ui_controller.gd
+++ screens/gameplay/ui/gameplay_ui_controller.gd
@@ -1,4 +1,9 @@
 extends CanvasLayer
 class_name GameplayUIController
 
-onready var buildPanel: BuildPanel = $VBoxContainer/CenterContainer/BuildPanel
+onready var buildPanel: BuildPanel = $VBoxContainer/BuildPanel
+onready var actionPrompt = $ActionPrompt
+
+func display_action_prompt(inputPosition: Vector2) -> void:
+  actionPrompt.set_global_position(inputPosition)
+  actionPrompt.isVisible = true
