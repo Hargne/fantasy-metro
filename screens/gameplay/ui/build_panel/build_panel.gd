7388d9864c1af8a80e6422ee445f9c5074e30432
Johan Hargne <johan.hargne@gmail.com>
2023-01-19 12:20:37 +0100

    Added StockButton

--- screens/gameplay/ui/build_panel/build_panel.gd
+++ screens/gameplay/ui/build_panel/build_panel.gd
@@ -3,71 +3,33 @@ class_name BuildPanel
 
 signal started_dragging_object(buildOption)
 # Refs
-onready var warehouseButtonContainer = $GridContainer/Warehouses
-onready var warehouseButton = $GridContainer/Warehouses/ButtonContainer/Button
-onready var warehouseAmountLabel = $GridContainer/Warehouses/AmountContainer/CenterContainer/Label
-onready var routeButtonContainer = $GridContainer/Routes
-onready var routeButton = $GridContainer/Routes/ButtonContainer/Button
-onready var routeAmountLabel = $GridContainer/Routes/AmountContainer/CenterContainer/Label
-onready var cartButtonContainer = $GridContainer/Carts
-onready var cartButton = $GridContainer/Carts/ButtonContainer/Button
-onready var cartAmountLabel = $GridContainer/Carts/AmountContainer/CenterContainer/Label
+onready var leftContainer = $HBoxContainer/Left
+onready var rightContainer = $HBoxContainer/Right
 
 func _ready():
-  Utils.connect_signal(warehouseButton, "button_down", self, "initiate_dragging_warehouse")
+  Utils.connect_signal(get_stockbutton_by_type(GameplayEnums.BuildOption.WAREHOUSE).button, "button_down", self, "initiate_dragging_warehouse")
 
-func get_container_by_type(buildOption) -> Node2D:
-  match buildOption:
-    GameplayEnums.BuildOption.WAREHOUSE:
-      return warehouseButtonContainer
-    GameplayEnums.BuildOption.ROUTE:
-      return routeButtonContainer
-    GameplayEnums.BuildOption.CART:
-       return cartButtonContainer
-  return null
-
-func get_button_by_type(buildOption) -> Node2D:
-  match buildOption:
-    GameplayEnums.BuildOption.WAREHOUSE:
-      return warehouseButton
-    GameplayEnums.BuildOption.ROUTE:
-      return routeButton
-    GameplayEnums.BuildOption.CART:
-        return cartButton
-  return null
-
-func get_amountlabel_by_type(buildOption) -> Node2D:
-  match buildOption:
-    GameplayEnums.BuildOption.WAREHOUSE:
-      return warehouseAmountLabel
-    GameplayEnums.BuildOption.ROUTE:
-      return routeAmountLabel
-    GameplayEnums.BuildOption.CART:
-        return cartAmountLabel
+func get_stockbutton_by_type(buildOptionType) -> StockButton:
+  for container in [leftContainer, rightContainer]:
+    for c in container.get_children():
+      if c.name.to_upper() == GameplayEnums.BuildOption.keys()[buildOptionType]:
+        return c
   return null
 
 func hide_build_option(buildOption) -> void:
-  var container = get_container_by_type(buildOption)
-  if container:
-    container.visible = false
+  var stockButton = get_stockbutton_by_type(buildOption)
+  if stockButton:
+    stockButton.visible = false
     
 func show_build_option(buildOption) -> void:
-  var container = get_container_by_type(buildOption)
-  if container:
-    container.visible = true
+  var stockButton = get_stockbutton_by_type(buildOption)
+  if stockButton:
+    stockButton.visible = true
 
 func set_build_option_amount(buildOption, amount) -> void:
-  var button = get_button_by_type(buildOption)
-  if button:
-    if amount > 0 && button.disabled:
-      button.disabled = false
-      button.modulate.a = 1
-    elif amount <= 0 && !button.disabled:
-      button.disabled = true
-      button.modulate.a = 0.25
-  var amountLabel = get_amountlabel_by_type(buildOption)
-  if amountLabel:
-    amountLabel.text = "x" + str(amount)
+  var btn = get_stockbutton_by_type(buildOption)
+  if btn:
+    btn.set_stock_amount(amount)
 
 # Stock should consist of a BuildOption as key coupled with a value: { BuildOption.CART: 1 }
 func update_stock(stock) -> void:
--- screens/gameplay/ui/gameplay_ui_controller.gd
+++ screens/gameplay/ui/gameplay_ui_controller.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 class_name GameplayUIController
 
 onready var buildPanel: BuildPanel = $VBoxContainer/BuildPanel
-onready var actionPrompt = $ActionPrompt
+onready var actionPrompt = $VBoxContainer/PlayArea/ActionPrompt
 
 func display_action_prompt(inputPosition: Vector2) -> void:
   actionPrompt.set_global_position(inputPosition)
