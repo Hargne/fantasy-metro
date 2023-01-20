b2d4c27cb0301d8746a20bfa689282bef01fe6d8
Johan Hargne <johan.hargne@gmail.com>
2023-01-20 11:12:59 +0100

    Fixed issue with MainMenu

--- screens/main_menu/main_menu.gd
+++ screens/main_menu/main_menu.gd
@@ -1,10 +1,5 @@
 extends Node2D
 
-var application_manager
-
-func _ready():
-  application_manager = get_node("/root/ApplicationManager")
-
 func start_game() -> void:
   if get_tree().change_scene("res://screens/gameplay/gameplay.tscn") != OK:
     printerr("Could not start game")
