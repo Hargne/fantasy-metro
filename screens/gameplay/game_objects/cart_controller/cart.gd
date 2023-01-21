e4cac405234145a31b1072b37d2049ed32d1d5e0
mjacobsca <mjacobsca@yahoo.com>
2023-01-20 21:05:12 -0800

    Started cart movement

--- screens/gameplay/game_objects/cart_controller/cart.gd
+++ screens/gameplay/game_objects/cart_controller/cart.gd
@@ -3,13 +3,32 @@ class_name Cart
 
 onready var storedResources = $ResourceList
 
-var defaultCartSpeed = 1.0
+var defaultCartSpeed = .1
 
 export var capacity = 1
-export var destinationPt = Vector2(0,0)
-export var cartSpeed = 1
 export var startPt = Vector2(0,0)
+export var destinationPt = Vector2(0,0)
+export var cartSpeed = .1
 var currentRoute: Route
+var currentStatus = RouteStatus.IN_PROGRESS
+
+var arrivalTime = 0
+var arrivalDelay = 500 # we use a .5 second delay for actions to take place
+
+enum RouteStatus {
+  IN_PROGRESS,
+  JUST_ARRIVED,
+  LOADING,
+  UNLOADING,
+  WAITING,
+  EXITING
+}
+
+func _process(_delta):
+  if currentStatus != RouteStatus.IN_PROGRESS || position.distance_to(destinationPt) < .05: # this is a magic number for now - probably will need to be based on map size or screen size or something
+    do_destination_handling()
+  else:
+    move_cart_to_destination()    
 
 func has_capacity() -> bool:
   return storedResources.resources.size() < capacity
@@ -46,4 +65,28 @@ func place_on_route(route: Route) -> void:
   cartSpeed = defaultCartSpeed
 
   look_at(destinationPt)
+
+func move_cart_to_destination() -> void:
+  position = position.move_toward(destinationPt, cartSpeed)
+
+func do_destination_handling() -> void:  
+  if currentStatus == RouteStatus.IN_PROGRESS:
+    currentStatus = RouteStatus.JUST_ARRIVED
+    arrivalTime = Time.get_ticks_msec()
+  elif currentStatus == RouteStatus.JUST_ARRIVED:
+    var t = Time.get_ticks_msec()
+
+    if (t - arrivalTime) > arrivalDelay:      
+      begin_destination_action()
+
+# in progress......
+func begin_destination_action() -> void:
+  if !storedResources.resources.empty():
+    true # unload
+
+  var destinationNode = currentRoute.get_map_node_from_point(destinationPt)
+
+  
+
+
   
--- screens/gameplay/game_objects/cart_controller/cart_controller.gd
+++ screens/gameplay/game_objects/cart_controller/cart_controller.gd
@@ -6,32 +6,32 @@ var carts = []
 var objectBeingPlaced: Node
 var canPlaceObject = false
 var typeOfObjectBeingPlaced
-var cartPrefab = preload("res://screens/gameplay/game_objects/map_node/cart/cart.tscn")
+var cartPrefab = preload("res://screens/gameplay/game_objects/cart_controller/cart.tscn")
 
 onready var placeCartSFX = $PlaceCartSFX
 
 func initiate_place_new_object(objectTypeToBePlaced, startPosition: Vector2) -> void:
-  typeOfObjectBeingPlaced = objectTypeToBePlaced
-  match objectTypeToBePlaced:
-    GameplayEnums.BuildOption.CART:
-      var cart = cartPrefab.instance()
-      add_child(cart)
-      objectBeingPlaced = cart	
-  if objectBeingPlaced:
-    objectBeingPlaced.position = startPosition
+	typeOfObjectBeingPlaced = objectTypeToBePlaced
+	match objectTypeToBePlaced:
+		GameplayEnums.BuildOption.CART:
+			var cart = cartPrefab.instance()
+			add_child(cart)
+			objectBeingPlaced = cart	
+	if objectBeingPlaced:
+		objectBeingPlaced.position = startPosition
 
 func end_place_new_object(route: Route) -> int:
-  if canPlaceObject:
-    if typeOfObjectBeingPlaced == GameplayEnums.BuildOption.CART:
-      objectBeingPlaced.place_on_route(route)
-      carts.append(objectBeingPlaced)
-      placeCartSFX.play()
-  else:
-    # Cancel placement by removing the new object
-    if objectBeingPlaced:
-      objectBeingPlaced.queue_free()
-  return canPlaceObject
+	if canPlaceObject:
+		if typeOfObjectBeingPlaced == GameplayEnums.BuildOption.CART:
+			objectBeingPlaced.place_on_route(route)
+			carts.append(objectBeingPlaced)
+			placeCartSFX.play()
+	else:
+		# Cancel placement by removing the new object
+		if objectBeingPlaced:
+			objectBeingPlaced.queue_free()
+	return canPlaceObject
 
 func stop_placing_object() -> void:
-  objectBeingPlaced = null
-  typeOfObjectBeingPlaced = null
+	objectBeingPlaced = null
+	typeOfObjectBeingPlaced = null
