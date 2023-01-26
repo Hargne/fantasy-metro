f65a17c06b8ab721fe2cd05f3156622ce8c1c77b
mjacobsca <mjacobsca@yahoo.com>
2023-01-25 16:13:28 -0800

    Cart deleting

--- screens/gameplay/game_objects/cart_controller/cart_controller.gd
+++ screens/gameplay/game_objects/cart_controller/cart_controller.gd
@@ -3,12 +3,19 @@ class_name CartController
 
 var carts = []
 
+signal on_increase_stock(item, amount)
+signal on_decrease_stock(item, amount)
+
 var objectBeingPlaced: Node
 var canPlaceObject = false
 var typeOfObjectBeingPlaced
 var cartPrefab = preload("res://screens/gameplay/game_objects/cart_controller/cart.tscn")
 
 onready var placeCartSFX = $PlaceCartSFX
+onready var demolishSFX = $DemolishSFX
+
+func _ready():
+  pass
 
 func initiate_place_new_object(objectTypeToBePlaced, startPosition: Vector2) -> void:
   typeOfObjectBeingPlaced = objectTypeToBePlaced
@@ -25,6 +32,7 @@ func end_place_new_object(connection: Connection) -> int:
     if typeOfObjectBeingPlaced == GameplayEnums.BuildOption.CART:
       objectBeingPlaced.place_on_connection(connection)
       carts.append(objectBeingPlaced)
+      Utils.connect_signal(objectBeingPlaced, "on_demolish", self, "demolish_cart")
       placeCartSFX.play()
     stop_placing_object()      
   else:
@@ -37,3 +45,15 @@ func end_place_new_object(connection: Connection) -> int:
 func stop_placing_object() -> void:
   objectBeingPlaced = null
   typeOfObjectBeingPlaced = null
+
+func blur_all_carts(except: Cart = null) -> void:
+  for cart in carts:
+    if (cart == null || cart != except):
+      cart.blur()
+
+func demolish_cart(cart) -> void:
+  if is_instance_valid(cart):
+    emit_signal("on_increase_stock", GameplayEnums.BuildOption.CART)
+    cart.queue_free()
+    carts.erase(cart)
+  demolishSFX.play()
