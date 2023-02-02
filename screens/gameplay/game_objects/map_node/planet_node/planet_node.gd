5a5b7427aa11d51ea18a2cf72be691804c184543
Johan Hargne <johan.hargne@gmail.com>
2023-02-02 21:31:18 +0100

    Update planet_node.gd

--- screens/gameplay/game_objects/map_node/planet_node/planet_node.gd
+++ screens/gameplay/game_objects/map_node/planet_node/planet_node.gd
@@ -3,7 +3,7 @@ class_name PlanetNode
 func get_class(): return "PlanetNode"
 
 onready var travellers = $TravellerList
-export var maxPassengers = 6
+export var maxTravellers = 6
 
 func _draw():
   draw_circle(position, 16, Color("#0000ff"))
@@ -12,13 +12,13 @@ func get_connection_point() -> Vector2:
   return Vector2(position.x, position.y)
 
 func can_add_traveller() -> bool:
-  return travellers.resources.size() < maxPassengers
+  return travellers.resources.size() < maxTravellers
 
-func add_passenger(travellerType) -> void:
+func add_traveller(travellerType) -> void:
   if can_add_traveller():
     travellers.add_resource(travellerType)
     
-func remove_passenger(travellerType) -> void:
+func remove_traveller(travellerType) -> void:
   if travellers.resources.size() > 0:
     travellers.remove_resource(travellerType)
 
